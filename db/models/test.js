738852c461b8e0cf41c6038462161721343bd82b
Ako Sorensen <akosorensen@gmail.com>
2020-11-22 17:47:13 -0500

    completed cycle-1

--- db/models/test.js
+++ db/models/test.js
@@ -1,7 +1,16 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
-const Student = require('./student');
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Student = require("./student");
 
-const Test = db.define();
+const Test = db.define("test", {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
 
 module.exports = Test;
